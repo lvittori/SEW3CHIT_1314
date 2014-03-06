d72be94006436eb78de0d0ff6295a627f6d099c6
Daniel Kocsi <kocsidaniel@gmail.com>
2014-03-06 16:27:51 +0100

    insertet system.out.println with name

--- HelloWorld.java
+++ HelloWorld.java
@@ -3,5 +3,6 @@ public class HelloWorld {
 		System.out.println("Hello World!");
 		System.out.println("Daniel Melichar");
 		System.out.println("Manfred Hofbauer");
+		System.out.println("Daniel Kocsi");
 	}
 }

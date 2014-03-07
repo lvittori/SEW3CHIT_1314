4e3f74e65276233f3d6d35ce24858b3198d834b9
Patrick1490 <l0l_patrick_l0l@yahoo.de>
2014-03-07 12:07:31 +0100

    irgendwas

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,11 +1,10 @@
-public class HelloWorld {
-	public static void main(String[] args) {
 		System.out.println("Hello World!");
 		System.out.println("Daniel Melichar");
 		System.out.println("Manfred Hofbauer");
 		System.out.println("Daniel Kocsi");
 		System.out.println("Eren Sefer");
 		System.out.println("Oezsoy Ahmet");
-	
+		System.out.println("Danny Hammerschmidt");
+		System.out.println("Patrick Wichert");
 	}
 }
